d7e26846986e6b6f7c24df7600b39cff6b812011
TaylorZ27 <taylorzavala85@gmail.com>
2026-02-19 01:11:19 -0500

    meow

--- SeniorDev_Project_BeachGame/Assets/Scenes/Scripts/SideScroller/scrollCamFollow.cs
+++ SeniorDev_Project_BeachGame/Assets/Scenes/Scripts/SideScroller/scrollCamFollow.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
+using System.Collections;
+
 
 public class scrollCamFollow : MonoBehaviour
 {
+    public Transform followPlayer;
+
+    public float smoothSpeed;
+
+    public Vector3 offsetPos;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +20,19 @@ public class scrollCamFollow : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void LateUpdate()
+    {
+        if (followPlayer != null)
+        {
+            Vector3 desiredPos = followPlayer.position + offsetPos;
+
+            Vector3 smoothPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed);
+
+            followPlayer.position = smoothPos;
+
+        }
+
     }
 }
