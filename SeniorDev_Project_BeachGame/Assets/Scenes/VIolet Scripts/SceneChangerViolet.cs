5cf3c8ceb5043709f481f4262f616566ac7dfc6c
TaylorZ27 <taylorzavala85@gmail.com>
2026-03-11 06:40:20 -0400

    Hub and Title music added + transitions

--- SeniorDev_Project_BeachGame/Assets/Scenes/VIolet Scripts/SceneChangerViolet.cs
+++ SeniorDev_Project_BeachGame/Assets/Scenes/VIolet Scripts/SceneChangerViolet.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,12 +6,37 @@ public class SceneChangerViolet : MonoBehaviour
 {
     public string sceneToLoad;
 
+    public static SceneChangerViolet instance;  
+    [SerializeField] Animator transitionAnim;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            NextLevel();
+            //WaitForSeconds(3);
             SceneManager.LoadScene(sceneToLoad);
             
         }
+    }
+
+    public void NextLevel()
+    {
+        StartCoroutine(LoadLevel1());
+    }
+
+    IEnumerator LoadLevel1()
+    {
+        transitionAnim.SetTrigger("End");
+        yield return new WaitForSeconds(3);
+        //SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        transitionAnim.SetTrigger("Start");
+    }
+
+    private Animator GetEndAnim()
+    {
+        GetComponent<Animator>();
+
+        return transitionAnim;
     }
 }
