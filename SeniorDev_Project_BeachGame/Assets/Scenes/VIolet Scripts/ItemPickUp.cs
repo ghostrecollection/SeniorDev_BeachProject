43ca9edcc846cc9f7513141015ef0b7e4b0af362
Violet <56025490+a2777@users.noreply.github.com>
2026-03-12 10:30:04 -0400

    Sounds Added

--- SeniorDev_Project_BeachGame/Assets/Scenes/VIolet Scripts/ItemPickUp.cs
+++ SeniorDev_Project_BeachGame/Assets/Scenes/VIolet Scripts/ItemPickUp.cs
@@ -3,11 +3,18 @@ using UnityEngine;
 public class ItemPickUp : MonoBehaviour
 {
     public ItemSO item; 
+    public AudioSource collectSound;
+
+    void Start()
+    {
+        //collectSound = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            collectSound.Play();
             ItemManager.instance.CollectItem(item);
             Destroy(gameObject);
         }
